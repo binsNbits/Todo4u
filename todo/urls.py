26e3bb945da241c916b164234dcdba06d1005585
binsNbits <jsrrsj85@gmail.com>
2023-11-10 16:40:31 -0500

    edit task functionality

--- todo/urls.py
+++ todo/urls.py
@@ -4,6 +4,15 @@ from . import views
 # place the url paths here 
 
 urlpatterns = [
+    # Add task feature
     path('addTask/', views.addTask, name='addTask'), 
-     path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done'),
+
+    # Mark as done feature
+    path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done'),
+
+    # mark as undone feature
+    path('mark_as_undone/<int:pk>/', views.mark_as_undone, name='mark_as_undone'),
+
+    # Edit feature
+    path('edit_task/<int:pk>', views.edit_task, name='edit_task'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -10,9 +10,25 @@ def addTask(request):
     Task.objects.create(task=task)
     return redirect('home')
 
+    
 def mark_as_done(request, pk):
     task = get_object_or_404(Task, pk=pk)
     task.is_completed = True
     task.save()
-    print(task)
     return redirect('home')
+
+def mark_as_undone(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    Task.objects.create(task=task)
+    task.is_completed = False
+    return redirect('home')
+
+def edit_task(request, pk):
+    get_task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        return
+    else:
+        context = {
+            'get_task' : get_task
+        }
+    return render(request, 'edit_task.html', context)
