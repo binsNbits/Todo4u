3145dbf5807a2bbbfa4d1a6d95af5156ca314cc1
binsNbits <jsrrsj85@gmail.com>
2023-11-13 09:55:32 -0500

    delete task feature

--- todo/urls.py
+++ todo/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
     # Mark as done feature
     path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done'),
 
-    # mark as undone feature
-    path('mark_as_undone/<int:pk>/', views.mark_as_undone, name='mark_as_undone'),
-
     # Edit feature
-    path('edit_task/<int:pk>', views.edit_task, name='edit_task'),
+    path('edit_task/<int:pk>/', views.edit_task, name='edit_task'),
+
+    #Delete task feature
+    path('delete_task/<int:pk>/', views.delete_task, name='delete_task'),
 ]
